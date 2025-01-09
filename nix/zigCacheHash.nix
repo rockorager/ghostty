1d6d52a7bf51a3afb4a57abd62eaeb5dcf2720c7
Tim Culverhouse <tim@timculverhouse.com>
2025-01-08 20:58:50 -0600

    cli: add +boo command
    Add a `+boo` command to show the animation from the website. The data
    for the frames is compressed during the build process. This build step
    was added to the SharedDeps object because it is used in both
    libghostty and in binaries.
    
    The compression is done as follows:
    - All files are concatenated together using \x01 as a combining byte
    - The files are compressed to a cached build file
    - A zig file is written to stdout which `@embedFile`s the compressed
      file and exposes it to the importer
    - A new anonymous module "framedata" is added in the SharedDeps object
    
    Any file can import framedata and access the compressed bytes via
    `framedata.compressed`. In the `boo` command, we decompress the file and
    split it into frames for use in the animation.
    
    The overall addition to the binary size is 348k.

--- nix/zigCacheHash.nix
+++ nix/zigCacheHash.nix
@@ -1,3 +1,3 @@
 # This file is auto-generated! check build-support/check-zig-cache-hash.sh for
 # more details.
-"sha256-Nx1tOhDnEZ7LVi/pKxYS3sg/Sf8TAUXDmST6EtBgDoQ="
+"sha256-S8kS+gO17dl9LJGKL1+kgDUre+vPTmdTvXzgc585Fl8="
