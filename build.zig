89a78c05122ca01ed5ec36663a80a3de50744c70
Gabriel Dinner-David <gabydinnerdavid@gmail.com>
2025-01-13 02:41:28 -0500

    make buildpkg pub

--- build.zig
+++ build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const builtin = @import("builtin");
-const buildpkg = @import("src/build/main.zig");
+pub const buildpkg = @import("src/build/main.zig");
 
 comptime {
     buildpkg.requireZig("0.13.0");
